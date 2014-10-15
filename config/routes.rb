dc4aaea13b6a73299f91b83b4eb747e9ee8dc137
Dustin <dustin@dquam.net>
2014-10-14 22:34:25 -0500

    initialized posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 BlogPostApi::Application.routes.draw do
+
+  # The project instructions indicate that we aren't destroying blog posts or 
+  # categories, so restrict those abilities.
+  with_options except: :destroy do |no_destroy|
+    no_destroy.resources :posts
+    no_destroy.resources :categories
+  end
+
+  # Below, we define a specific API namespace so our model/controller naming 
+  # conventions don't get wild.
+
+  # We're also restricting access to the API through the api.* 
+  # subdomain for better load balancing options at the DNS level.
+  # (This requires editing the HOSTS file in dev environments)
+  namespace :api, constraints:{ subdomain: 'api' } do
+    resources :posts
+    resources :categories
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
