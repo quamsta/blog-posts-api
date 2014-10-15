385be34a05ce89aea19cabd49382194db85ff66c
Dustin <dustin@dquam.net>
2014-10-14 20:57:20 -0500

    initial commit: models initiated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPostApi::Application.config.secret_token = 'f805089cf2dfb71ead66ba907f927509382af0a006396770aec99eb9a50ac3f87092b004772ff8a2e94a75bc1ff660acd1cae6c72a1dbb9b2f2bb4f3aff2bbca'
